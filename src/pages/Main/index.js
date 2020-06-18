ca2d3c3da683198003956aa51a83074038c70b4b
Bruno <bgoldoni@valid.com.br>
2020-06-18 18:55:57 -0300

    feat - Integration with API Github

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,19 +1,58 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import { Keyboard, Alert } from 'react-native';
+
+import api from '../../services/api';
 
 import Icon from 'react-native-vector-icons/MaterialIcons';
 
 import { Container, Form, Input, SubmitButton } from './styles';
 
 const Main = () => {
+    const [users, setUsers] = useState([]);
+    const [newUser, setNewUser] = useState('');
+
+    const handleAddUser = async () => {
+        let userData = users;
+
+        try {
+            const response = await api.get(`/users/${newUser}`);
+
+            const data = {
+                bio: response.data.bio,
+                name: response.data.name,
+                login: response.data.login,
+                avatar: response.data.avatar_url,
+            };
+
+            userData.push(data);
+
+            setUsers(userData);
+
+            setNewUser('');
+        } catch (err) {
+            Alert.alert(
+                'Ops... Algo deu errado!',
+                'Verifique os dados e tente novamente.'
+            );
+        }
+
+        Keyboard.dismiss();
+    };
+
     return (
         <Container>
             <Form>
                 <Input
+                    value={newUser}
                     autoCorrect={false}
+                    returnKeyType="send"
                     autoCapitalize="none"
+                    onSubmitEditing={handleAddUser}
                     placeholder="Adicionar usuário"
+                    onChangeText={(text) => setNewUser(text)}
                 />
-                <SubmitButton>
+                <SubmitButton onPress={handleAddUser}>
                     <Icon name="add" size={20} color="#FFF" />
                 </SubmitButton>
             </Form>
