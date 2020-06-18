2f454009f058254c46dba9ed542b456badf08185
Bruno <bgoldoni@valid.com.br>
2020-06-18 17:58:48 -0300

    feat - React Navigation
    -react-navigaton 5x

--- src/index.js
+++ src/index.js
@@ -1,31 +1,9 @@
 import React from 'react';
 
+import Routes from './routes';
+
 import './config/ReactotronConfig';
 
-import { StyleSheet, View, Text } from 'react-native';
-
-const styles = StyleSheet.create({
-    container: {
-        alignItems: 'center',
-        backgroundColor: '#F5FcFF',
-        flex: 1,
-        justifyContent: 'center',
-    },
-    wellcome: {
-        fontSize: 20,
-        fontWeight: 'bold',
-        textAlign: 'center',
-    },
-});
-
-console.tron.log('Hello Bruno');
-
-const App = () => {
-    return (
-        <View style={styles.container}>
-            <Text style={styles.wellcome}>Wellcome to React Native</Text>
-        </View>
-    );
-};
-
-export default App;
+export default function App() {
+    return <Routes />;
+}
---
+++ src/routes.js
@@ -0,0 +1,50 @@
+import * as React from 'react';
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import Main from './pages/Main';
+import User from './pages/User';
+
+const Routes = () => {
+    const Stack = createStackNavigator();
+
+    return (
+        <NavigationContainer>
+            <Stack.Navigator initialRouteName="Main">
+                <Stack.Screen
+                    name="User"
+                    component={User}
+                    options={{
+                        headerTitleStyle: {
+                            flexGrow: 1,
+                            textAlign: 'center',
+                            alignSelf: 'center',
+                        },
+                        headerStyle: {
+                            backgroundColor: '#7159c1',
+                        },
+                        headerTintColor: '#FFF',
+                    }}
+                />
+                <Stack.Screen
+                    name="Main"
+                    component={Main}
+                    options={{
+                        headerTitleStyle: {
+                            flexGrow: 1,
+                            textAlign: 'center',
+                            alignSelf: 'center',
+                        },
+                        headerStyle: {
+                            backgroundColor: '#7159c1',
+                        },
+                        headerTintColor: '#FFF',
+                    }}
+                />
+            </Stack.Navigator>
+        </NavigationContainer>
+    );
+};
+
+export default Routes;
