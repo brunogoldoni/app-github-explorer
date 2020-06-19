e57eefa0a04b37c6affd9866ac05f185fa09a7e2
Bruno <bgoldoni@valid.com.br>
2020-06-19 10:44:57 -0300

    fix - route parms

--- src/pages/User/index.js
+++ src/pages/User/index.js
@@ -1,23 +1,17 @@
 import React from 'react';
 
-import PropTypes from 'prop-types';
-
-// import { useRoute } from '@react-navigation/native';
+import { useRoute } from '@react-navigation/native';
 
 import { Container } from './styles';
 
-const User = ({ navigation }) => {
-    // const route = useRoute();
+const User = () => {
+    const route = useRoute();
 
-    console.tron.log('PARAMS', navigation);
+    const user = route.params.user;
 
-    return <Container></Container>;
+    console.tron.log('USER', user);
+
+    return <Container />;
 };
 
 export default User;
-
-User.PropTypes = {
-    navigation: PropTypes.shape({
-        navigate: PropTypes.func,
-    }).isRequired,
-};
