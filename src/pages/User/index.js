587b10cd6b1e8b264073f1e8bc48f6490ddf7438
Bruno <bgoldoni@valid.com.br>
2020-06-19 14:59:57 -0300

    feat - searching api data

--- src/pages/User/index.js
+++ src/pages/User/index.js
@@ -1,15 +1,28 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 import { useRoute } from '@react-navigation/native';
+
+import api from '../../services/api';
 
 import { Container } from './styles';
 
 const User = () => {
     const route = useRoute();
-
     const user = route.params.user;
 
-    console.tron.log('USER', user);
+    const [stars, setStars] = useState();
+
+    const getStars = async () => {
+        try {
+            const response = await api.get(`/users/${user.login}/starred`);
+
+            setStars(response.data);
+        } catch (err) {}
+    };
+
+    useEffect(() => {
+        getStars();
+    }, []);
 
     return <Container />;
 };
